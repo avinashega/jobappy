7eb78d67690ed122f298a101cec43fdb9de72f12
Avinash Ega <avinash.ega@booking.com>
2017-12-05 22:46:48 +0100

    done for today

--- phq9.js
+++ phq9.js
@@ -3,21 +3,21 @@
 module.exports = function() {
     return {
         questions: [
-            '1/9 Little interest or pleasure in doing things?',
-            '2/9 Feeling down, depressed, or hopeless?',
-            '3/9 Trouble falling or staying asleep, or sleeping too much?',
-            '4/9 Feeling tired or having little energy?',
-            '5/9 Poor appetite or overeating?',
-            '6/9 Feeling bad about yourself - or that you are a failure or have let yourself or your family down?',
-            '7/9 Trouble concentrating on things, such as reading the newspaper or watching television?',
-            '8/9 Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?',
-            '9/9 Thoughts that you would be better off dead, or of hurting yourself in some way?'
+            '1/9. Little interest or pleasure in doing things?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '2/9. Feeling down, depressed, or hopeless?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '3/9. Trouble falling or staying asleep, or sleeping too much?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '4/9. Feeling tired or having little energy?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '5/9. Poor appetite or overeating?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '6/9. Feeling bad about yourself - or that you are a failure or have let yourself or your family down?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '7/9. Trouble concentrating on things, such as reading the newspaper or watching television?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '8/9. Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day',
+            '9/9. Thoughts that you would be better off dead, or of hurting yourself in some way?\n1. Not at All\n2. Several Days\n3. More than half the Days\n4. Nearly every day'
         ],
         options: [
-            {title:'Not at All', score:0},
-            {title:'Several Days', score:1},
-            {title:'More than half the Days', score:2},
-            {title:'Nearly every day', score:3}
+            {title:'1', score:0},
+            {title:'2', score:1},
+            {title:'3', score:2},
+            {title:'4', score:3}
         ]
     }
 }
